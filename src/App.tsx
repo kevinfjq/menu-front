e11b5ebba7e1a34f080da1000472aa5f1942c105
kevinfjq <kevifjq@gmail.com>
2024-01-30 11:52:25 -0300

    style: remove unnused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Card } from './components/card/card';
-import { FoodData } from './interface/FoodData';
 import { useFoodData } from './hooks/useFoodData';
 import { CreateModal } from './components/create-modal/create-modal';
 
